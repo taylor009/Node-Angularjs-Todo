6c88962f53966d222c52cb0f27706149c9cfb52d
taylor009 <taylor@baremetalcloud.com>
2019-01-05 10:59:28 -0500

    Finished server configuration file

--- server.js
+++ server.js
@@ -6,5 +6,25 @@ var port     = process.env.PORT || 8080;
 var database = require('./config/database');
 
 
-// Log request to the console
-var morgan = require('morgan');
+
+var morgan         = require('morgan');            // Log request to the console
+var bodyParser     = require('body-parser');      // Pull information from HTML Post
+var methodOverride = require('method-override'); // Simulate DELETE and PUT
+
+
+//Configuration
+mongoose.connect(database.url);
+
+app.use(express.static(__dirname + '/public'));  // Set the static files location /public img will be /img for users
+app.use(morgan('dev'));                  // Log every request to the console
+app.use(bodyParser.urlencoded({'extended': true})); // Parser application/x-www-form-urlencoded
+app.use(bodyParser.json());                        // parse application/json
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // Parser application/vmd.api+json as json
+
+// Routes
+require('./app/routes.js');
+
+
+//listen (start app with node server.js)
+app.listen(port);
+console.log('App is listening on port ' + port);
